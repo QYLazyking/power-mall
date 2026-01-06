f99f670837dca63b60838a6db62ac8c23a9ba63a
qingyang <zguangzhan@163.com>
2026-01-06 22:13:35 +0800

    认证服务模块添加依赖、配置

---
+++ cloud-iaas/auth-server/src/main/java/cn/lazyking/power/AuthServerApplication.java
@@ -0,0 +1,14 @@
+package cn.lazyking.power;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 认证服务模块启动类
+ */
+@SpringBootApplication
+public class AuthServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+}

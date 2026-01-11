5ee4e32d74b29eba2e32d534134588490701f458
qingyang <zguangzhan@163.com>
2026-01-11 15:40:54 +0800

    多条件分页查询系统角色列表

--- power-business/manager-service/src/main/java/cn/lazyking/power/controller/SysRoleController.java
+++ power-business/manager-service/src/main/java/cn/lazyking/power/controller/SysRoleController.java
@@ -3,11 +3,16 @@ package cn.lazyking.power.controller;
 import cn.lazyking.power.domain.SysRole;
 import cn.lazyking.power.model.Result;
 import cn.lazyking.power.service.impl.SysRoleService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,6 +35,25 @@ public class SysRoleController {
 
         return Result.ok(roles);
     }
+
+    @ApiOperation("多条件分页查询系统角色列表")
+    @GetMapping("page")
+    @PreAuthorize("hasAuthority('sys:role:page')")
+    public Result<Page<SysRole>> loadSysRolePage(
+            @RequestParam Long current,
+            @RequestParam Long size,
+            @RequestParam(required = false) String roleName
+    ) {
+        Page<SysRole> page = new Page<>(current, size);
+        page = sysRoleService.page(
+                page,
+                new LambdaQueryWrapper<SysRole>()
+                        .like(StringUtils.hasText(roleName), SysRole::getRoleName, roleName)
+                        .orderByDesc(SysRole::getCreateTime)
+        );
+        return Result.ok(page);
+    }
+
 
 
 

8196d3a5b0460b3e584e1b5548fcfeaf9fa5bcbe
qingyang <zguangzhan@163.com>
2026-01-11 11:22:34 +0800

    多条件分页查询系统管理员

--- power-business/manager-service/src/main/java/cn/lazyking/power/controller/SysUserController.java
+++ power-business/manager-service/src/main/java/cn/lazyking/power/controller/SysUserController.java
@@ -4,11 +4,16 @@ import cn.lazyking.power.domain.SysUser;
 import cn.lazyking.power.model.Result;
 import cn.lazyking.power.service.impl.SysUserService;
 import cn.lazyking.power.utils.AuthUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -32,6 +37,28 @@ public class SysUserController {
         // 移除密码
         sysUser.setPassword(null);
         return Result.ok(sysUser);
+    }
+
+    @ApiOperation("多条件分页查询系统管理员列表")
+    @GetMapping("page")
+    @PreAuthorize("hasAuthority('sys:user:page')")
+    public Result<Page<SysUser>> loadSysUserPage(
+            @RequestParam Long current,
+            @RequestParam Long size,
+            @RequestParam(required = false) String username
+    ) {
+        // 多条件分页查询系统管理员
+        // 构建 page 对象
+        Page<SysUser> page = new Page<>(current, size);
+        LambdaQueryWrapper<SysUser> queryWrapper = new LambdaQueryWrapper<>();
+        // 查询条件，当username 非空时，添加查询条件
+        queryWrapper.like(
+                StringUtils.hasText(username),
+                SysUser::getUsername,
+                username
+        ).orderByDesc(SysUser::getCreateTime);
+        page = sysUserService.page(page, queryWrapper);
+        return Result.ok(page);
     }
 
 }
